3e83a5efe10fb23666354daf6df65042c02416dc
riulwoo <geun10121@naver.com>
2025-10-09 23:07:07 +0900

    [build] 번들링 파일 분리 및 캐시 처리

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/',
-    clean: true // 빌드 전에 output 디렉토리를 정리
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
+      chunkFilename: isProduction ? '[name].[contenthash].chunk.js' : '[name].chunk.js',
+      publicPath: '/',
+      clean: true
+    },
+    // 코드 스플리팅 최적화
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+          common: {
+            name: 'common',
+            minChunks: 2,
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      },
+      runtimeChunk: 'single',
+      usedExports: true,
+      sideEffects: false,
+    },
+    // 성능 경고 설정 조정
+    performance: {
+      hints: isProduction ? 'warning' : false,
+      maxEntrypointSize: 500000,
+      maxAssetSize: 300000,
+    },
   cache: {
     type: 'filesystem', // 파일시스템 기반 캐시
     buildDependencies: {
@@ -31,11 +63,20 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          {
+            loader: 'style-loader',
+            options: {
+              injectType: 'singletonStyleTag', // 하나의 style 태그에 모든 CSS 주입
+              attributes: {
+                id: 'budget-app-styles'
+              }
+            }
+          },
           {
             loader: 'css-loader',
             options: {
               sourceMap: true, // 소스맵 활성화
+              importLoaders: 1
             }
           }
         ]
@@ -59,8 +100,9 @@ module.exports = {
     hot: true, // 핫 모듈 교체
     liveReload: true, // 라이브 리로드
     watchFiles: ['src/**/*.css', 'src/**/*.js'], // 파일 변경 감지
-    proxy: {
-      '/api': 'http://localhost:3000'
+      proxy: {
+        '/api': 'http://localhost:3000'
+      }
     }
-  }
+  };
 }; 
